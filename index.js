461b1924c00b8b099af9276e590624ee6b584918
khamalismadie <ismadiepro@gmail.com>
2022-06-03 21:21:27 +0700

    Tugas 4

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (date, index = null) => {
+  let arrayDate = [];
+
+  date.forEach((date) => {
+    arrayDate.push(new Date(date).getTime() / 1000);
+});
+
+if (index === null) {
+  return arrayDate.sort((a, b) => a - b).join("\n");
+} else {
+  return `${new Date(dates[index]).getTime() / 1000}`;
+}
 
 // ! JANGAN DIMODIFIKASI
 (() => {
